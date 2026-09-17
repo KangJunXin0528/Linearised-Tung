8e05f57c3a3ae7924826ba0a820e42f810068159
Ethan-Hau-Lintian <ethanabcde5@gmail.com>
2026-09-17 14:38:46 +0800

    Outline inputs, outputs, and main menu structure

---
+++ Other-Prototypes/formula_lineariser.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <cmath>     
+#include <limits>    
+#include <cctype>    
+#include <thread>    
+#include <chrono>    
+
+using namespace std;
+
+int main() {
+    cout << "Formula Lineariser Initialized.\n";
+    return 0;
+}
